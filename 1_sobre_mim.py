0da18aa3fdb910e7198bbbf5120a2073b9e416d4
ILSGI <144830299+ILSGI@users.noreply.github.com>
2023-11-09 08:05:44 -0300

    Update 1_sobre_mim.py

--- 1_sobre_mim.py
+++ 1_sobre_mim.py
@@ -1,37 +1,43 @@
-import streamlit as st 
+import streamlit as st
+from streamlit_timeline import timeline
+import json
+from PIL import Image
+import io
 
 st.set_page_config(
-    page_title="Lincoln - sobre mim",
+    page_title="Lincoln - Linha do Tempo",
     page_icon="☭"
 )
- 
-idioma = st.sidebar.selectbox("escolha o idioma", ['pt', 'en', 'es'])
 
+idioma = st.sidebar.selectbox("escolha o idioma", ['pt', 'en', 'es'])
 
 strings = {
     "en": {
-        "titulo": "About me",
-        "texto": "Hello, I'm Lincoln, a young man born in 2017. Every story has its first step, I want to start mine in data science. I have two passions in my life: sports and technology. Both of these passions have taught me many things: sports help me with teamwork, drive, discipline, and how to love and deal with challenges. On the technology side, I've learned to acquire information, deal with stress, and think logically. I believe that this portfolio is the first step towards a transformative future, uniting passion and career is the dream of many and I believe I can achieve it."
+        "titulo": "timeline",
     },
     "pt": {
-        "titulo": "Sobre mim",
-        "texto": "Olá, sou o Lincoln, um jovem nascido em 2017. Toda história tem seu primeiro passo, desejo iniciar a minha na ciência de dados. Tenho duas paixões na minha vida: esportes e tecnologia. Ambas as histórias têm me ensinado diversas coisas: os esportes me ajudam com trabalho em equipe, disposição, disciplina e como amar e lidar com desafios. Por parte da tecnologia: aprendi a adquirir informação, lidar com estresse e a pensar de maneira lógica. Acredito que esse portfólio é o primeiro passo em direção a um futuro transformador, unir paixão e carreira é o sonho de muitos e acredito poder realizá-lo."
+        "titulo": "linha do tempo",
     },
     "es": {
-        "titulo": "Sobre mí",
-        "texto": "Hola, soy Lincoln, un joven nacido en 2017. Cada historia tiene su primer paso, deseo comenzar la mía en ciencia de datos. Tengo dos pasiones en mi vida: los deportes y la tecnología. Ambas pasiones me han enseñado muchas cosas: los deportes me ayudan con el trabajo en equipo, el impulso, la disciplina y cómo amar y lidiar con los desafíos. En el lado de la tecnología, he aprendido a adquirir información, lidiar con el estrés y pensar de manera lógica. Creo que este portafolio es el primer paso hacia un futuro transformador, unir pasión y carrera es el sueño de muchos y creo que puedo lograrlo."
+        "titulo": "línea de tiempo",
     }
 }
 
-
 def get_string(idioma, key):
     return strings[idioma][key]
-  
-  
+
 titulo = get_string(idioma,"titulo")
 st.title(titulo)
 
-st.subheader("Lincoln Sales e Gonçalves")
+if idioma == "en" : 
+    with open ('dados_lincoln_en.json', "r", encoding="utf-8") as json_timeline:
+        timeline_info = json.load(json_timeline)     
+elif idioma == "es" :
+    with open ('dados_lincoln_es.json', "r", encoding="utf-8") as json_timeline:
+        timeline_info = json.load(json_timeline) 
+elif idioma == "pt" : 
+    with open ('dados_lincoln_pt.json', "r", encoding="utf-8") as json_timeline:
+        timeline_info = json.load(json_timeline) 
 
-texto = get_string(idioma, "texto")
-st.write(texto)
+
+timeline(timeline_info, height=500, )

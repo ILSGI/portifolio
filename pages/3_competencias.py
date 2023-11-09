7752abbe0296f8f1e214974d1a18529e418fb9f5
ILSGI <144830299+ILSGI@users.noreply.github.com>
2023-11-09 19:06:03 -0300

    Update 3_competencias.py

--- pages/3_competencias.py
+++ pages/3_competencias.py
@@ -12,17 +12,23 @@ strings = {
     "en": {
         "titulo": "Competencies",
         "python": "As one of the languages I use, Python helps me a lot in its ease of automation and visualization through Streamlit, as in this portfolio. I also have knowledge of different libraries such as: NumPy, Pandas, Streamlit, etc.",
-        "sql": "Structured query language (SQL) is the language used to manipulate, verify, and manage databases/relational databases. I use this language in the vast majority of my projects."
+        "sql": "Structured query language (SQL) is the language used to manipulate, verify, and manage databases/relational databases. I use this language in the vast majority of my projects.",
+        "ingles": "english",
+        "espanhol": "Spanish"
     },
     "pt": {
         "titulo": "Competências",
         "python": "Como uma das linguagens que utilizo, o Python me auxilia muito em sua facilidade para automações e visualização por meio do Streamlit, como nesse portfólio. Também tenho conhecimento de diferentes bibliotecas como: NumPy, Pandas, Streamlit, etc.",
-        "sql": "Structured query language (SQL) é a linguagem usada para manipular, verificar e gerenciar BDs/SGBDs. Utilizo essa linguagem na grande maioria dos meus projetos."
+        "sql": "Structured query language (SQL) é a linguagem usada para manipular, verificar e gerenciar BDs/SGBDs. Utilizo essa linguagem na grande maioria dos meus projetos.",
+        "ingles": "inglês",
+        "espanhol": "espanhol"
     },
     "es": {
         "titulo": "Competencias",
         "python": "Como una de las lenguajes que utilizo, Python me ayuda mucho en su facilidad para automatizaciones y visualización a través de Streamlit, como en este portafolio. También tengo conocimiento de diferentes bibliotecas como: NumPy, Pandas, Streamlit, etc.",
-        "sql": "El lenguaje de consulta estructurado (SQL) es el lenguaje utilizado para manipular, verificar y administrar bases de datos/sistemas de gestión de bases de datos relacionales. Utilizo este lenguaje en la gran mayoría de mis proyectos."
+        "sql": "El lenguaje de consulta estructurado (SQL) es el lenguaje utilizado para manipular, verificar y administrar bases de datos/sistemas de gestión de bases de datos relacionales. Utilizo este lenguaje en la gran mayoría de mis proyectos.",
+        "ingles": "Inglés",
+        "espanhol": "Español"
     }
 }
 
@@ -40,9 +46,6 @@ with st.expander("python"):
 with st.expander("sql"):
     sql = get_string(idioma, "sql")
     st.write(sql)
-    
-with st.expander("vou add mais"):
-    "pedir ajuda a fernanda"
     
 with st.expander("ingles"):
     "pedir ajuda a fernanda"
